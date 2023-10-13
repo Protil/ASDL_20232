ccb1a71034bf7a0314e67123785974cff80aa7e6
David Protil <dzprotil@gmail.com>
2023-10-13 14:18:06 -0300

    Questão 1 completa

--- Lista_1/Q1_1.m
+++ Lista_1/Q1_1.m
@@ -8,7 +8,6 @@ R = 1; % Valor do Resistor
 
 L = 1; % Valor do Indutor
 
-
 T = L / R / 1e2; % Mantém a relação do intervalo de tempo estabelecida inicialmente no circuito RC
 
 syms y(t);
@@ -17,40 +16,58 @@ Dy = diff(y);
 
 y = dsolve(1 == L/R *Dy + y, y(0) == 0);
 
-t1 = 0:T: 5 * L/R;
+t1 = 0:T: 5 * L/R; % Array dos intervalos para calcular o grafico das funções
 
 y1 = subs(y, t, t1);
-
 figure(1);
-
 subplot(2, 2, 1);
-
 plot(t1, y1);
+xlabel('Tempo (s)');
+ylabel('Tensão de Saída (V)');
+title('Resposta ao Degrau Diferencial');
 
-xlabel('tempo (s)');
-ylabel('tensão de saída (V)');
-title('Resposta ao degrau');
+grid;
 
-grid
-
-
-y = 1*(1 - exp(-(R/L)*t)); 
-
-t1 = 0:T: 5 * L/R;
-
-y1 = subs(y, t, t1)
+% Função exponencial informada no estudo dirigido 
+y_exp = 1*(1 - exp(-(R/L)*t)); 
+y_exp_sub = subs(y_exp, t, t1);
 
 subplot(2, 2, 2);
+plot(t1, y_exp_sub);
+xlabel('Tempo (s)');
+ylabel('Tensão de Saída (V)');
+title('Resposta ao Degrau Exponencial');
 
-plot(t1, y1);
+grid;
+
+% Grafico de ambas as funções sobrepostas
+subplot(2, 2, 3);
+plot(t1, y1, t1, y_exp_sub);
+legend('Diferencial', 'Exponencial')
+xlabel('Tempo (s)');
+ylabel('Tensão de Saída (V)');
+title('Diferencial e Exponencial Sobrepostas');
+grid;
+
+% Grafico da diferença entre as funções
+subplot(2, 2, 4);
+plot(t1, y1 - y_exp_sub);
+xlabel('Tempo (s)');
+ylabel('Tensão de Saída (V)');
+title('Diferença Diferencial e Exponencial');
+grid;
+
+
+func_iguais = isequal(y, y_exp); % Compara as duas funções e retorna 1 se forem iguais, senão 0
+
+% Display no console da igualdade entre funções 
+if (func_iguais)
+    disp("As funções são iguais");
+else 
+    disp("As funções são diferentes");
+end
 
 
 
 
 
-
-
-
-
-
-grid
